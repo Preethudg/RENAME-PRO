9c3188ee0688ffea7ac2d738fc200dae902a750c
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-15 12:03:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7824911074:AAEk7FKLig0x48wL3Evzw7-pAxJ5Gx6_lh4")
+API_ID = int(os.environ.get("API_ID", "23009724"))
+API_HASH = os.environ.get("API_HASH", "66a87d259426e268bdd765fb4c635f2b")
+OWNER = int(os.environ.get("OWNER", "2089142494"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Autocaptiondhmgaut_Bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "kannadamovieskannadadubb")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002334874259"))
 
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
